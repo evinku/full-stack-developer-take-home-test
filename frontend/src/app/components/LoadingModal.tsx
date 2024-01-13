c9e328396016b6bfaaf80d67e3c9ae2942574fd5
Kevin Kuhfeldt <kevin.kuhfeldt@gmx.de>
2024-01-13 16:36:45 +0400

    fix z-index of spinner

--- frontend/src/app/components/LoadingModal.tsx
+++ frontend/src/app/components/LoadingModal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingModal: React.FC = () => {
     return (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-20">
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
         </div>
     );
